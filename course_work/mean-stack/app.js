430e490a1d5f3c0a8d5913ad0ad22b3b5145828b
Binghui Liu <bertieliu52@gmail.com>
2020-05-28 20:09:50 +0300

    angular build to public

--- course_work/mean-stack/app.js
+++ course_work/mean-stack/app.js
@@ -34,6 +34,10 @@ app.get('/', (req, res) => {
     res.send("invalid endpoint")
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 app.listen(port, () => {
     console.log("server starts on port " + port)
 })
