7d8a61f1a1203331e10de78ee140ea2578c36611
Binghui Liu <bertieliu52@gmail.com>
2020-06-07 17:47:34 +0300

    display blog posts in grid

--- project/mean-stack/angular-src/src/app/components/blog/blog.component.ts
+++ project/mean-stack/angular-src/src/app/components/blog/blog.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  blogs: any
 
   constructor() { }
 
   ngOnInit(): void {
+    this.blogs = [
+      {
+        "title": "One"
+      },
+      {
+        "title": "Two"
+      },
+      {
+        "title": "Three"
+      }
+    ]
   }
 
 }
